580b33d46da2b7fbe20808e1ce0e725c2f8af919
sasipreya <124431279+Viewnaruk@users.noreply.github.com>
2025-10-22 13:08:30 +0700

    first commit

---
+++ APP/main.py
@@ -0,0 +1,20 @@
+import uvicorn ##ASGI
+from fastapi import FastAPI
+import os
+
+# 2. Create the app object
+app = FastAPI()
+
+# 3. Index route, opens automatically on http://127.0.0.1:8000
+@app.get('/')
+def index():
+    return {'message': 'Hello, World'}
+
+# 5. Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+
+
+if __name__ == '__main__':
+    uvicorn.run("APP.main:app", host='0.0.0.0', port=int(os.getenv('PORT', 9000)))
+
+
